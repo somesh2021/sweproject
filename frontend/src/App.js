58b63693ef4c6812eb7e6b5fc0958176d007e954
somesh2021 <somlok2002@gmail.com>
2023-02-27 13:19:36 -0500

    updated App.js with contact

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/HomePage/Home";
 import About from "./pages/AboutPage/About";
+import Contact from "./pages/Contact/Contact"
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
+      <Route path="/Contact" element={<Contact/>} />
     </Routes>
   );
 }
