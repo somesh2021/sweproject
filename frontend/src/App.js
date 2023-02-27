9224a1ff02679ee2df279809886ab9e89ad58208
samirroshan <samir.roshan@yahoo.com>
2023-02-27 13:33:35 -0500

    Made orders page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,15 @@ import "./App.css";
 import Home from "./pages/HomePage/Home";
 import About from "./pages/AboutPage/About";
 import Contact from "./pages/Contact/Contact"
+import Orders from "./pages/Orders/orders"
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
       <Route path="/Contact" element={<Contact/>} />
+      <Route path="/Orders" element={<Orders/>} />
+
     </Routes>
   );
 }
