dc41200898461a74b56dfc5371d98c2a03a551f2
Samir <samir.roshan@yahoo.com>
2023-04-04 15:09:49 -0400

    update

--- frontend/src/pages/HomePage/Home.js
+++ frontend/src/pages/HomePage/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Home.css";
+import LoginSignUp from "./pages/LoginSignUpPage/LoginSignUp";
 import CustomNavbar from "../../components/Navigation/Navigation";
 const Home = () => {
   return (
     <CustomNavbar/>
+    
   );
 };
 
